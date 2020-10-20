d0e842ac15f80192ee95265b63c8d32d78fa4bf3
DE LA CASA Aurélien <aurelien@fridaa.co>
2020-10-20 19:24:46 +0200

    fodid

--- lib/time_manager/store/user.ex
+++ lib/time_manager/store/user.ex
@@ -9,10 +9,13 @@ defmodule TimeManager.Store.User do
     timestamps()
   end
 
-  @doc false
+@doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:username, :email])
-    |> validate_required([:username, :email])
+    |> cast(attrs, [:email, :username])
+    |> validate_required([:email, :username])
+    |> unique_constraint(:email)
+    |> unique_constraint(:username)
+    |> validate_format(:email, ~r/.*\@.*\..*/)
   end
 end
